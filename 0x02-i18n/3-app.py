a79b1f2f95f174e1af250f98e7a1053a3af21afe
edison pryce <edisonpryce@gmail.com>
2024-11-06 03:40:09 +0000

    Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+This script creates a get_locale function with a
+babel.localeselector decorator
+"""
+
+from flask_babel import _
+from flask import request, render_template
+from typing import Optional
+
+Config = __import__('1-app').Config
+app = __import__('0-app').app
+babel = __import__('1-app').babel
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> Optional[str]:
+    """This function determines the best match for
+    supported languages.
+    """
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route('/')
+def home() -> str:
+    """Rendering the homepage with the
+    translated languages.
+    """
+    return render_template('3-index.html')
